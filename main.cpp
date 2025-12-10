1f04de80ef2fb9633ea640474827bf3c64d6a75d
paXen-code <vika.fir.2006@gmail.com>
2025-12-10 09:09:48 +0500

    функция 1

--- main.cpp
+++ main.cpp
@@ -19,6 +19,16 @@ int main()
 {
     //функция 1:
     wifstream inFile("ababa.txt");
+    vector<shaurma> vse_shaurmy;
+    while(!inFile.eof())
+    {
+        shaurma vid;
+        inFile >> vid.kalorii;
+        inFile >> vid.ves;
+        inFile >> vid.sous;
+        inFile >> vid.meat;
+        vse_shaurmy.push_back(vid);
+    }
 
     //функция 2:
 
